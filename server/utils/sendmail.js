00f672504c1f30798dfc859b7ace090803160f0c
Arjun Prajapati <arjunprajapati3786@gmail.com>
2023-04-11 21:59:24 +0530

    initial commit

--- server/utils/sendmail.js
+++ server/utils/sendmail.js
@@ -32,3 +32,34 @@ module.exports = function (email, subject, message) {
     }
   });
 };
+
+
+// require("dotenv").config;
+// const nodemailer = require("nodemailer");
+
+// module.exports = function (email, subject, message) {
+//   let mailTransporter = nodemailer.createTransport({
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     auth: {
+//         user: 'isai.simonis@ethereal.email',
+//         pass: 'W67d7qSsGX2nwAM1Ww',
+//     }
+//   });
+
+//   let mailDetails = {
+//     from: "isai.simonis@ethereal.email",
+//     to: email,
+//     subject: subject,
+//     text: message,
+//   };
+
+//   mailTransporter.sendMail(mailDetails, function (err, data) {
+//     if (err) {
+//       console.log(err);
+//       console.log("Error Occurs");
+//     } else {
+//       console.log("Email sent successfully");
+//     }
+//   });
+// };
